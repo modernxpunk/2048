c29df3d1ef1fe21905a65d247f9f7f5b310b8f24
modernxpunk <modernxpunk@gmail.com>
2021-08-09 18:20:52 +0300

    fiix

---
+++ config.js
@@ -0,0 +1,86 @@
+const config = {
+	'': {
+		styles: {
+			innerHTML: '',
+			color: '',
+			backgroundColor: '#cdc1b4'
+		}
+	},
+	2: {
+		styles: {
+			fontSize: '3.25em',
+			color: "#776e65",
+			backgroundColor: '#eee4da'
+		}
+	},
+	4: {
+		styles: {
+			fontSize: '3.25em',
+			color: "#776e65",
+			backgroundColor: '#eee1c9'
+		}
+	},
+	8: {
+		styles: {
+			fontSize: '3em',
+			color: "#f9f6f2",
+			backgroundColor: '#f3b27a'
+		}
+	},
+	16: {
+		styles: {
+			fontSize: '3em',
+			color: "#f9f6f2",
+			backgroundColor: '#f69664'
+		}
+	},
+	32: {
+		styles: {
+			fontSize: '3em',
+			color: "#f9f6f2",
+			backgroundColor: '#f77c5f'
+		}
+	},
+	64: {
+		styles: {
+			fontSize: '3em',
+			color: "#f9f6f2",
+			backgroundColor: '#f75f3b'
+		}
+	},
+	128: {
+		styles: {
+			fontSize: '3em',
+			color: "#f9f6f2",
+			backgroundColor: '#edd073'
+		}
+	},
+	256: {
+		styles: {
+			fontSize: '3em',
+			color: "#f9f6f2",
+			backgroundColor: '#edcc61'
+		}
+	},
+	512: {
+		styles: {
+			fontSize: '3em',
+			color: "#f9f6f2",
+			backgroundColor: '#edc850'
+		}
+	},
+	1024: {
+		styles: {
+			fontSize: '2.25em',
+			color: "#f9f6f2",
+			backgroundColor: '#edc53f'
+		}
+	},
+	2048: {
+		styles: {
+			fontSize: '2.25em',
+			color: "#f9f6f2",
+			backgroundColor: '#edc22e'
+		}
+	}
+}
